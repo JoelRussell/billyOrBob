c37e73e7f6b2724ddec06918800daff6cb829c5b
Joel Russell <joel@joel.pm>
2021-02-22 05:52:00 +0000

    CU-emxyhd - Deliver corpus list to selectors in UI

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,37 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace billyOrBob.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration Configuration;
 
         // TODO Reference actual files
-        public string[] ShakespeareFiles { get; private set; } = {
-            "much_afoo_abar_nothing",
-            "foolius_ceabar",
-            "midwinter_nights_code"
-        };
-        public string[] BurnsFiles { get; private set; } = {
-            "much_afoo_abar_nothing",
-            "foolius_ceabar",
-            "midwinter_nights_code"
-        };
+        public string[] ShakespeareFiles { get; private set; } = {};
+        public string[] BurnsFiles { get; private set; } = {};
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
+            ShakespeareFiles = fileNames(Configuration["ShakespeareCorpus"]);
+            BurnsFiles = fileNames(Configuration["BurnsCorpus"]);
         }
 
         public void OnGet()
         {
 
 
+        }
+
+        public string[] fileNames(string directory) {
+            return new DirectoryInfo(directory).GetFiles().Select(f => f.Name).ToArray();
         }
     }
 }
